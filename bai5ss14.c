a477b13d9c9d6fd6192a3b2449be8b977bdbf27d
NotHuyXD <n24dtcn038@student.ptithcm.edu.vn>
2024-12-31 08:53:19 +0700

    Add files via upload

--- bai5ss14.c
+++ bai5ss14.c
@@ -3,5 +3,14 @@
 int main(){
     char c[]= "Chào mừng bạn đến với Rikkei Education";
     int count=0;
-    
+    int length = strlen(c);
+
+    for (int i = 0; i < length; i++) {
+        if ((i == 0 || c[i-1] == ' ') && c[i] != ' ') {
+            count++;
+        }
+    }
+
+    printf("Số từ trong chuỗi là: %d\n", count);
+    return 0;
 }
